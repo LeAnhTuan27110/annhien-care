58aaec0f68ebd493f9e2ddfad2f3ce05eab66a3e
kaido <leanhtuan02072006@gmail.com>
2026-07-23 03:39:00 +0700

    chore: add controller authorization support

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 /** Base controller for shared HTTP behavior as application modules are added. */
 abstract class Controller
 {
+    use AuthorizesRequests;
 }
